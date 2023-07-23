6fffd636a0f7d92dbc35a717e793457b0a286268
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-07-23 16:31:26 -0300

    feat(class-1): add uiState and viewModel

--- app/src/main/java/com/filipe1309/expensecontrol/MainActivity.kt
+++ app/src/main/java/com/filipe1309/expensecontrol/MainActivity.kt
@@ -1,30 +1,51 @@
 package com.filipe1309.expensecontrol
 
 import android.os.Bundle
-import android.view.SurfaceControl
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.ViewModel
 import com.filipe1309.expensecontrol.ui.theme.ExpenseControlTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class MyViewModel: ViewModel() {
+    data class UiState(
+        val transactions: List<String> = emptyList()
+    )
+
+    private val _state = MutableStateFlow(UiState())
+    val uiState: StateFlow<UiState> = _state
+
+    fun add(transaction: String) {
+        val currentTransactions = _state.value.transactions.toMutableList()
+        currentTransactions.add(transaction)
+        _state.value = UiState(currentTransactions)
+    }
+
+
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,35 +81,37 @@ fun Welcome(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun Transactions(modifier: Modifier = Modifier) {
-    LazyColumn(
-        modifier = modifier
-            .padding(16.dp)
-            .fillMaxWidth(),
-        contentPadding = PaddingValues(8.dp)
-    ) {
-        items(transactionsDummy.size) { index ->
-            Transaction(modifier, index)
+fun Transactions(modifier: Modifier = Modifier, viewModel: MyViewModel = viewModel()) {
+    val uiState by viewModel.uiState.collectAsState()
+    Column {
+        LazyColumn(
+            modifier = modifier
+                .padding(16.dp)
+                .weight(1f)
+                .fillMaxWidth(),
+            contentPadding = PaddingValues(8.dp)
+        ) {
+            items(uiState.transactions.size) { index ->
+                Transaction(uiState.transactions[index])
+            }
+        }
+
+        Button(onClick = { viewModel.add("Transaction ${uiState.transactions.size + 1}") }) {
+            Text(text = "Add Transaction")
         }
     }
 }
 
 @Composable
-private fun Transaction(modifier: Modifier, index: Int) {
+private fun Transaction(transaction: String, modifier: Modifier = Modifier) {
     Card(
         elevation = CardDefaults.cardElevation(8.dp),
         modifier = modifier
             .fillMaxWidth()
-            .padding(16.dp)
     ) {
         Row {
-            Icon(
-                imageVector = Icons.Filled.Settings,
-                contentDescription = "Settings"
-            )
-            Spacer(modifier = modifier.padding(16.dp))
             Text(
-                text = transactionsDummy[index],
+                text = transaction,
                 modifier = Modifier
                     .padding(8.dp)
                     .fillMaxWidth(),
@@ -98,14 +121,3 @@ private fun Transaction(modifier: Modifier, index: Int) {
         }
     }
 }
-
-private val transactionsDummy = listOf<String>(
-    "Gasolina", "Aluguel", "Estudos", "Café", "Academia",
-    "Gasolina", "Aluguel", "Estudos", "Café", "Academia",
-    "Gasolina", "Aluguel", "Estudos", "Café", "Academia",
-    "Gasolina", "Aluguel", "Estudos", "Café", "Academia",
-    "Gasolina", "Aluguel", "Estudos", "Café", "Academia",
-    "Gasolina", "Aluguel", "Estudos", "Café", "Academia",
-    "Gasolina", "Aluguel", "Estudos", "Café", "Academia",
-    "Gasolina", "Aluguel", "Estudos", "Café", "Academia",
-)
