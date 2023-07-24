7e3671cce7c183293dcfd84dd2151ec357747452
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-07-24 17:43:34 -0300

    feat(class-3): add button to scroll to top

--- app/src/main/java/com/filipe1309/expensecontrol/OverviewScreen.kt
+++ app/src/main/java/com/filipe1309/expensecontrol/OverviewScreen.kt
@@ -7,8 +7,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowCircleUp
 import androidx.compose.material.icons.filled.ClearAll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -22,12 +24,14 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.filipe1309.expensecontrol.ui.theme.formatDate
 import com.filipe1309.expensecontrol.ui.theme.randomTransaction
+import kotlinx.coroutines.launch
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -66,6 +70,8 @@ fun OverviewScreen(viewModel: OverviewViewModel = viewModel()) {
         }
     ) {
         val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+        val listState = rememberLazyListState()
+        val scope = rememberCoroutineScope()
         Column(Modifier.padding(it)) {
             Text(
                 modifier = Modifier.padding(start = 16.dp, end = 16.dp),
@@ -73,7 +79,8 @@ fun OverviewScreen(viewModel: OverviewViewModel = viewModel()) {
             )
             LazyColumn(
                 modifier = Modifier.padding(start = 16.dp, end = 16.dp),
-                contentPadding = PaddingValues(top = 16.dp, bottom = 16.dp)
+                contentPadding = PaddingValues(top = 16.dp, bottom = 16.dp),
+                state = listState
             ) {
                 items(uiState.transactions) { transaction ->
                     TransactionCard(
@@ -83,6 +90,15 @@ fun OverviewScreen(viewModel: OverviewViewModel = viewModel()) {
                         date = transaction.date.formatDate()
                     )
                     Spacer(modifier = Modifier.height(8.dp))
+                }
+                item {
+                    IconButton(onClick = {
+                        scope.launch {
+                            listState.scrollToItem(0)
+                        }
+                    }) {
+                        Icon(imageVector = Icons.Filled.ArrowCircleUp, contentDescription = "Scroll to top")
+                    }
                 }
             }
         }
