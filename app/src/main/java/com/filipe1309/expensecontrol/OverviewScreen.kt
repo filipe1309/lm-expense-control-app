03e682130fc6717111d1df27ea95817b706ab5aa
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-07-24 18:35:18 -0300

    feat(class-3): add buttons animations

--- app/src/main/java/com/filipe1309/expensecontrol/OverviewScreen.kt
+++ app/src/main/java/com/filipe1309/expensecontrol/OverviewScreen.kt
@@ -1,8 +1,19 @@
 package com.filipe1309.expensecontrol
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -22,9 +33,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -37,7 +51,12 @@ import kotlinx.coroutines.launch
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun OverviewScreen(viewModel: OverviewViewModel = viewModel()) {
-
+    val listState = rememberLazyListState()
+    val showButton by remember {
+        derivedStateOf {
+            listState.firstVisibleItemIndex > 0
+        }
+    }
     Scaffold(
         topBar = {
             TopAppBar(
@@ -65,13 +84,24 @@ fun OverviewScreen(viewModel: OverviewViewModel = viewModel()) {
             FloatingActionButton(onClick = {
                 viewModel.addTransaction(randomTransaction())
             }) {
-                Icon(imageVector = Icons.Filled.Add, contentDescription = "Add")
+                Row(
+                    Modifier.padding(horizontal = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(imageVector = Icons.Filled.Add, contentDescription = "Add")
+                    AnimatedVisibility(visible = !showButton) {
+                        Text(
+                            modifier = Modifier.padding(start = 8.dp),
+                            text = "Add"
+                        )
+                    }
+                }
             }
         }
     ) {
         val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-        val listState = rememberLazyListState()
         val scope = rememberCoroutineScope()
+
         Column(Modifier.padding(it)) {
             Text(
                 modifier = Modifier.padding(start = 16.dp, end = 16.dp),
@@ -91,14 +121,38 @@ fun OverviewScreen(viewModel: OverviewViewModel = viewModel()) {
                     )
                     Spacer(modifier = Modifier.height(8.dp))
                 }
-                item {
-                    IconButton(onClick = {
+            }
+        }
+        AnimatedVisibility(
+            visible = showButton,
+            enter = slideInVertically(
+                initialOffsetY = { fullHeight -> fullHeight },
+                animationSpec = tween(
+                    durationMillis = 500,
+                    easing = FastOutSlowInEasing
+                )
+            ) + fadeIn(),
+            exit = slideOutVertically { fullHeight -> fullHeight } + fadeOut(),
+        ) {
+            Box(Modifier.fillMaxSize()) {
+                IconButton(
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .padding(16.dp)
+                        .background(
+                            color = MaterialTheme.colorScheme.primary,
+                            shape = MaterialTheme.shapes.extraLarge
+                        ),
+                    onClick = {
                         scope.launch {
-                            listState.scrollToItem(0)
+                            listState.animateScrollToItem(0)
                         }
-                    }) {
-                        Icon(imageVector = Icons.Filled.ArrowCircleUp, contentDescription = "Scroll to top")
-                    }
+                }) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowCircleUp,
+                        tint = MaterialTheme.colorScheme.onPrimary,
+                        contentDescription = "Scroll to top",
+                    )
                 }
             }
         }
